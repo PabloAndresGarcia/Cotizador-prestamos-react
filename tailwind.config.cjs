9b72609b76ccccb25df89457a4c0f94064aad88e
Pablo Andres Garcia <garciasjs@gmail.com>
2023-01-03 19:09:51 -0300

    Fin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     './index.html',
     './src/**/*.{js, ts, jsx, tsx}',
-     "./src/App.jsx",
+    "./src/App.jsx",
      "./src/components/Header.jsx",
      "./src/components/Button.jsx"
   ],
